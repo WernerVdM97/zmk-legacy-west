narze/zmk

feature(split): add support for sensors from peripheral
This commit adds a new GATT characteristics on the peripheral side
and wires it up to read sensor values. The central side subscribes
to this new characteristics and replays sensor values on its side.

Co-authored-by: Peter Johanson <peter@peterjohanson.com>

app/include/zmk/split/bluetooth/service.h
@@ -6,7 +6,17 @@
 
 #pragma once
 
+#include <zmk/events/sensor_event.h>
+#include <zmk/sensors.h>
+
 #define ZMK_SPLIT_RUN_BEHAVIOR_DEV_LEN 9
+
+struct sensor_event {
+    uint8_t sensor_index;
+
+    uint8_t channel_data_size;
+    struct zmk_sensor_channel_data channel_data[ZMK_SENSOR_EVENT_MAX_CHANNELS];
+} __packed;
 
 struct zmk_split_run_behavior_data {
     uint8_t position;
@@ -22,3 +32,6 @@ struct zmk_split_run_behavior_payload {
 
 int zmk_split_bt_position_pressed(uint8_t position);
 int zmk_split_bt_position_released(uint8_t position);
+int zmk_split_bt_sensor_triggered(uint8_t sensor_number,
+                                  const struct zmk_sensor_channel_data channel_data[],
+                                  size_t channel_data_size);

WernerVdM97/zmk-legacy-west

app/include/zmk/split/bluetooth/service.h
@@ -6,7 +6,17 @@
 
 #pragma once
 
+#include <zmk/events/sensor_event.h>
+#include <zmk/sensors.h>
+
 #define ZMK_SPLIT_RUN_BEHAVIOR_DEV_LEN 9
+
+struct sensor_event {
+    uint8_t sensor_index;
+
+    uint8_t channel_data_size;
+    struct zmk_sensor_channel_data channel_data[ZMK_SENSOR_EVENT_MAX_CHANNELS];
+} __packed;
 
 struct zmk_split_run_behavior_data {
     uint8_t position;
@@ -22,3 +32,6 @@ struct zmk_split_run_behavior_payload {
 
 int zmk_split_bt_position_pressed(uint8_t position);
 int zmk_split_bt_position_released(uint8_t position);
+int zmk_split_bt_sensor_triggered(uint8_t sensor_index,
+                                  const struct zmk_sensor_channel_data channel_data[],
+                                  size_t channel_data_size);